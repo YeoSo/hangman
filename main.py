f1d601592b0c2df3142fbe2372798aff2c73093d
YeoreumSong <yeoreumyg@gmail.com>
2024-05-03 13:20:26 +0900

    Minor changes

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from hangman_art import logo
 import random
 from hangman_words import word_list
+import string
 
+alphabet = list(string.ascii_lowercase)  # Generates a list of lowercase alphabets
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
-with open("chosen_word.txt", "w") as file:
-    file.write(chosen_word)
+try:
+    with open("chosen_word.txt", "w") as file:
+        file.write(chosen_word)
+except Exception as e:
+    print(f"An error occurred while writing the chosen word to file: {e}")
 
 end_of_game = False
 lives = 6
@@ -27,6 +32,10 @@ print(f"Welcome {name.title()}. You have 6 lives to try. Good luck!\n")
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    if guess not in alphabet:
+        print("Please enter a valid alphabet.")
+        continue  # Skip the rest of the loop and start over
+
     # If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
         print(f"You've already guessed {guess}")
@@ -46,7 +55,6 @@ while not end_of_game:
         lives -= 1
         if lives == 0:
             end_of_game = True
-            print("You lose.")
 
     # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -54,9 +62,20 @@ while not end_of_game:
     # Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
-        print("You win.")
 
     # Import the stages from hangman_art.py and make this error go away.
     from hangman_art import stages
 
     print(stages[lives])
+    if lives == 1:
+        print(f"You have {lives} life left.")
+    elif lives == 0:
+        print(f"You have no more life left.")
+    else:
+        print(f"You have {lives} lives left.")
+
+# Check if user has got all letters.
+if "_" not in display:
+    print(f"You win! Well done {name}")
+else:
+    print("Game Over!")
